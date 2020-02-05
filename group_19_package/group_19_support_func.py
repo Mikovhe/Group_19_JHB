eae5cb16d072f5c07adfe17fa3b4afcae0b15e77
Mikovhe <mukovhe.physics@gmail.com>
2020-02-05 22:38:22 +0200

    added switch and edited my_sort

--- group_19_package/group_19_support_func.py
+++ group_19_package/group_19_support_func.py
@@ -1,19 +1,27 @@
-def my_sort_func(data):
-    sorted_list = []
-    
-    while len(data)>0:
-        num = data[0]    
-        for num2 in data:
-            if num2 < num:
-                num = num2
+def my_sort(data):
+    '''
+        Sorts items in a list by comparing consecutive numbers,
+        if number at index i is smaller than number at i-1,
+        the function switches those numbers.
 
-        sorted_list.append(num)
-        data.remove(num)
-   
-    return sorted_list 
-    
+    '''
 
-def sumvalues(numlist):
+    sorting = True
+    while sorting:
+        data_copy = tuple(data)
+        for i in range(1,len(data)):
+            j = i-1
+
+            if data[i]<data[j]:
+                data = switch(data,i,j)
+
+        if data_copy == tuple(data):
+            sorting =False
+
+    return(data)
+
+
+def my_sum(numlist):
   total=0
   for num in numlist:
     total+=num
@@ -21,8 +29,8 @@ def sumvalues(numlist):
 
 
 def my_len(data):
-    count = 0 
-    for item in data: 
+    count = 0
+    for item in data:
         count += 1
     return count
 
@@ -31,7 +39,24 @@ def my_median(num_list):
     if my_len(s_list)%2 == 1:
         ind = int(my_len(s_list)/2)
         return s_list[ind]
-   
+
     else:
-        ind = int(my_len(s_list)/2) 
+        ind = int(my_len(s_list)/2)
         return (s_list[ind]+s_list[ind-1])/2
+
+
+def switch(data,ind1,ind2):
+    '''
+        takes in a list and indices and switch places
+        of values at those indices
+
+    '''    
+    #store the values to memory
+    value_ind1 = data[ind1]
+    value_ind2 = data[ind2]
+
+    #assign values to new position
+    data[ind1] = value_ind2
+    data[ind2] = value_ind1
+
+    return data
