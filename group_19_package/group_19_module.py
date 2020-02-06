0e7e0d739645605ee893d53385963b450a65ff72
Mikovhe <mukovhe.physics@gmail.com>
2020-02-06 22:40:34 +0200

    edited func1

--- group_19_package/group_19_module.py
+++ group_19_package/group_19_module.py
@@ -3,13 +3,15 @@ from group_19_support_func import *
 def function_1(data):
     sorted_list = my_sort(data)
 
-    #mean = my_mean(data)
+    mean = my_mean(data)
     median = my_median(data)
     maximum = sorted_list[-1]
     minimum = sorted_list[0]
-    #std_dev = my_std(data)
-    #variance  = std_dev**0.5
+    variance  = my_var(data)
+    std_dev = variance**0.5
 
-    result_dict = {"mean":'missing','median':median,'maximum':maximum,'minimum':minimum,'standard deviation':'missing','variance':'missing'}
+    result_dict = {"mean":round(mean,2),'median':round(median,2),
+                    'maximum':round(maximum,2),'minimum':round(minimum,2),
+                    'standard deviation':round(std_dev,2),'variance':round(variance,2)}
 
     return result_dict
