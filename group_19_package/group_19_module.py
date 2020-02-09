c3a1c84bcb92095a4610e9e5e9f437a6f8e0b58c
Mikovhe <mukovhe.physics@gmail.com>
2020-02-09 16:10:41 +0200

    renamed function_1 to dictionary_of_metrics

--- group_19_package/group_19_module.py
+++ group_19_package/group_19_module.py
@@ -1,6 +1,6 @@
 from group_19_support_func import *
 
-def function_1(data):
+def dictionary_of_metrics(data):
     sorted_list = my_sort(data)
 
     mean = my_mean(data)
@@ -11,7 +11,7 @@ def function_1(data):
     std_dev = variance**0.5
 
     result_dict = {"mean":round(mean,2),'median':round(median,2),
-                    'maximum':round(maximum,2),'minimum':round(minimum,2),
-                    'standard deviation':round(std_dev,2),'variance':round(variance,2)}
+                    'standard deviation':round(std_dev,2),'variance':round(variance,2),
+                    'maximum':round(maximum,2),'minimum':round(minimum,2)}
 
     return result_dict
