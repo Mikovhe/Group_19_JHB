1882eba66ca770bf94795989a915cb6d593cc447
Mikovhe <mukovhe.physics@gmail.com>
2020-02-10 22:54:52 +0200

    added extract_municipality_hashtags function

--- group_19_package/group_19_module.py
+++ group_19_package/group_19_module.py
@@ -15,3 +15,34 @@ def dictionary_of_metrics(data):
                     'maximum':round(maximum,2),'minimum':round(minimum,2)}
 
     return result_dict
+
+def extract_municipality_hashtags(df):
+    # your code here
+    muni_per_tweet = []
+    hashtags_per_tweet = []
+
+    for tweet in df['Tweets']:
+        tweet_list = tweet.split()
+        hash_tags = [word.lower() for word in tweet_list if word[0]=='#']
+        handles = [word for word in tweet_list if word[0]=='@']
+
+        if hash_tags ==[]:
+            hash_tags = np.nan
+
+        municipality = np.nan
+
+        for handle in handles:
+            mun_handle = ''
+            for char in handle:
+                if mun_handle in mun_dict:
+                    municipality = mun_dict[mun_handle]
+
+                mun_handle +=char
+
+        muni_per_tweet.append(municipality)
+        hashtags_per_tweet.append(hash_tags)
+
+    df['municipality'] = muni_per_tweet
+    df['hashtags'] = hashtags_per_tweet
+
+    return df
