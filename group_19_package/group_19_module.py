6972d0648371eab0e6846657a36368bd3e769f14
Mikovhe <mjmukwevho@outlook.com>
2020-02-18 10:32:12 +0200

    added docstring extract_municipality_hashtags

--- group_19_package/group_19_module.py
+++ group_19_package/group_19_module.py
@@ -1,94 +1,97 @@
 import pandas as pd
 import numpy as np
 
+
+
+
+###################################################################################
+#support functions
+def my_sort(data):
+    sorting = True
+    while sorting:
+        data_copy = tuple(data)
+        for i in range(1,len(data)):
+            j = i-1
+
+            if data[i]<data[j]:
+                data = switch(data,i,j)
+
+        if data_copy == tuple(data):
+            sorting =False
+
+    return(data)
+
+def my_sum(numlist):
+    total=0
+    for num in numlist:
+        total+=num
+    return total
+
+
+def my_len(data):
+    count = 0
+    for _ in data:
+        count += 1
+    return count
+
+def my_median(num_list):
+    s_list = my_sort(num_list)
+    if my_len(s_list)%2 == 1:
+        ind = int(my_len(s_list)/2)
+        return s_list[ind]
+
+    else:
+        ind = int(my_len(s_list)/2)
+        return (s_list[ind]+s_list[ind-1])/2
+
+
+def switch(data,ind1,ind2):
+    '''
+        takes in a list and indices and switch places
+        of values at those indices
+
+    '''    
+    #store the values to memory
+    value_ind1 = data[ind1]
+    value_ind2 = data[ind2]
+
+    #assign values to new position
+    data[ind1] = value_ind2
+    data[ind2] = value_ind1
+
+    return data
+
+
+def my_mean(my_list):
+    '''
+    the function takes in a list of numeric values
+    then returns the mean of the  list.
+    '''
+    return(my_sum(my_list)/my_len(my_list))
+
+
+def my_var(nums):
+    '''
+    This function calculates the variance of the values in the list: nums
+    Remember to replace the sum and len functions with our own functions
+    '''
+    mean=my_mean(nums)
+    varnums=[(num-mean)**2 for num in nums]
+    varvalue=my_sum(varnums)/(my_len(nums)-1)
+    return varvalue
 ###################################################################################
 #function 1
 def dictionary_of_metrics(data):
     '''
-        Function takes a list of numbers as parameter and returns
-         a dictionary containing the mean, median, maximum, minimum, variance and the
-        standard deviation...
+    parameters
+    -----------
+    data: input list.
 
-        Example: dictionary_of_metrics([1,2,3,4,5]) == {"mean":3.0,"median":3.0,
-                    }
+    returns
+    ----------
+    a dictionary containing the mean, median, maximum, minimum, variance and the
+        standard deviation
     '''
-
-    def my_sort(data):
-        sorting = True
-        while sorting:
-            data_copy = tuple(data)
-            for i in range(1,len(data)):
-                j = i-1
-
-                if data[i]<data[j]:
-                    data = switch(data,i,j)
-
-            if data_copy == tuple(data):
-                sorting =False
-
-        return(data)
-
-    def my_sum(numlist):
-        total=0
-        for num in numlist:
-            total+=num
-        return total
-
-
-    def my_len(data):
-        count = 0
-        for _ in data:
-            count += 1
-        return count
-
-    def my_median(num_list):
-        s_list = my_sort(num_list)
-        if my_len(s_list)%2 == 1:
-            ind = int(my_len(s_list)/2)
-            return s_list[ind]
-
-        else:
-            ind = int(my_len(s_list)/2)
-            return (s_list[ind]+s_list[ind-1])/2
-
-
-    def switch(data,ind1,ind2):
-        '''
-            takes in a list and indices and switch places
-            of values at those indices
-
-        '''    
-        #store the values to memory
-        value_ind1 = data[ind1]
-        value_ind2 = data[ind2]
-
-        #assign values to new position
-        data[ind1] = value_ind2
-        data[ind2] = value_ind1
-
-        return data
-
-
-    def my_mean(my_list):
-        '''
-        the function takes in a list of numeric values
-        then returns the mean of the  list.
-        '''
-        return(my_sum(my_list)/my_len(my_list))
-
-
-    def my_var(nums):
-        '''
-        This function calculates the variance of the values in the list: nums
-        Remember to replace the sum and len functions with our own functions
-        '''
-        mean=my_mean(nums)
-        varnums=[(num-mean)**2 for num in nums]
-        varvalue=my_sum(varnums)/(my_len(nums)-1)
-        return varvalue
-
-
-
     sorted_list = my_sort(data)
 
     mean = my_mean(data)
@@ -132,7 +135,20 @@ def date_parser(dates):
 
 #function 4
 def extract_municipality_hashtags(df,mun_dict):
-    # your code here
+    '''
+        Function takes in a data frame and dictionary of 
+        municipalilties and returns the municipality 
+        mentioned in the tweet as well as the all the hashtags. 
+
+        parameters
+        ----------
+        df: pandas dataframe
+        mun_dict: municipality dictionary
+        
+        Returns
+        ----------
+        dataframe with added municipalities and hashtags fields. 
+    '''
     muni_per_tweet = []
     hashtags_per_tweet = []
 
