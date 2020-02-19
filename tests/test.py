7c68486c846d8f5a60bf321ed9ad34cf51dee809
Mikovhe <mjmukwevho@outlook.com>
2020-02-19 13:51:32 +0200

    edited test.py

--- tests/test.py
+++ tests/test.py
@@ -11,7 +11,7 @@ def test_dictionary_of_metric():
 
 #add tests for other functions
 def test_five_number_summary():
-    ```make sure test_five_number_summary works```
+    '''make sure test_five_number_summary works'''
 
     assert group_19_module.test_five_number_summary([1,2,3,4,5]) ==
     {'min': 1, 'q1': 2.0, 'median': 3.0, 'q3': 4.0, 'max': 5}
@@ -19,7 +19,7 @@ def test_five_number_summary():
     {'mean': 5.9, 'median': 5.5, 'std': 2.02, 'variance': 4.09, 'max': 9, 'min': 2}
 
 def test_date_parser():
-    ```make sure the date parser works```
+    '''make sure the date parser works'''
     assert group_19_module.date_parser(['2019-11-29 12:50:54',
     '2019-11-29 12:46:53',
     '2019-11-29 12:46:10']) == ['2019-11-29',
