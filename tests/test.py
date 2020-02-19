c76ee74df3fbe9adbf3bff5c7a3b2afae52f914c
Mikovhe <mjmukwevho@outlook.com>
2020-02-19 15:47:08 +0200

    edited gitignore, module and test

--- tests/test.py
+++ tests/test.py
@@ -8,8 +8,8 @@ from group_19_package import group_19_module
 def test_dictionary_of_metric():
     '''make sure dictionary_of_metrics works'''
 
-    assert group_19_module.dictionary_of_metrics([1,2,3,4,5])=={'mean':3.0,'median':3, 'std': 1.41,'variance':2.0,'max':5,'min':1}
-    assert group_19_module.dictionary_of_metrics([7, 8, 5, 2, 5, 9, 5, 4, 8, 6])=={'mean': 5.9, 'median': 5.5, 'std': 2.02, 'variance': 4.09, 'max': 9, 'min': 2}
+    assert group_19_module.dictionary_of_metrics([1,2,3,4,5])=={'mean':3.0,'median':3, 'std': 1.58,'variance':2.5,'max':5,'min':1}
+    assert group_19_module.dictionary_of_metrics([7, 8, 5, 2, 5, 9, 5, 4, 8, 6])=={'mean': 5.9, 'median': 5.5, 'std': 2.13, 'variance': 4.54, 'max': 9, 'min': 2}
 
 #test_dictionary_of_metric()
 #add tests for other functions
@@ -17,7 +17,7 @@ def test_five_number_summary():
     '''make sure test_five_number_summary works'''
 
     assert group_19_module.five_num_summary([1,2,3,4,5]) == {'min': 1, 'q1': 2.0, 'median': 3.0, 'q3': 4.0, 'max': 5}
-    assert group_19_module.dictionary_of_metrics([7, 8, 5, 2, 5, 9, 5, 4, 8, 6])=={'mean': 5.9, 'median': 5.5, 'std': 2.02, 'variance': 4.09, 'max': 9, 'min': 2}
+    assert group_19_module.five_num_summary([7, 8, 5, 2, 5, 9, 5, 4, 8, 6])=={'min': 2,'q1': 5.0, 'median':5.5,'q3': 7.75, 'max': 9}
 
 def test_date_parser():
     '''make sure the date parser works'''
@@ -42,10 +42,22 @@ dict1={
 }
 
 def test_extract_munipality_hashtags():
+    Tweets=['@thulamela #loadsheding is a problem #mukovhe do your job', '@ekhuruleni #izinyokaproblems bring our copper', '@polokwane #toomanytsotsi ba nyakile go nhlaba ka mphaka please help ','did jesus make it to northcliff']
+    Dates=['2019-02-23 12:56:53','2019-02-23 11:56:53','2018-05-23 12:56:53','2017-02-23 12:56:53']
+    df1={
+        'Tweets': Tweets,
+        'Dates': Dates
+    }
+    df=pd.DataFrame(df1)
+    dict1={
+        '@thulamela':'thulamela',
+        '@polokwane':'polokwane',
+        '@ekhuruleni':'ekhuruleni'
+    }
     result=df.copy()
-    result['municipality']= ['thulamela','ekhuruleni','polokwane', 'NaN']
-    result['hashtags']=[['#loadsheding','#mukovhe'], ['#izinyokaproblems'], ['#toomanytsotsi'],'NaN']
-    assert group_19_module.extract_munipality_hashtags(df,mun_dict = dict1)==result
+    result['municipality']= ['thulamela','ekhuruleni','polokwane', np.nan]
+    result['hashtags']=[['#loadsheding','#mukovhe'], ['#izinyokaproblems'], ['#toomanytsotsi'],np.nan]
+    assert group_19_module.extract_municipality_hashtags(df,dict1)==result
 
 def test_number_of_tweets_per_day():
     resu={
@@ -87,8 +99,3 @@ def test_stop_words_http_remover():
     stop_words_dict={'stopwords': ['problem', 'bring','make']}
     func7fd['Without Stop Words']=dm
     assert group_19_module.stop_words_http_remover(df)==func7fd
-
-
-
-
-test_date_parser()
